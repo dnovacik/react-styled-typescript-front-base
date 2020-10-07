41cde19d885d8829c3e49c060131e7ce6b1302ce
dnovacik <novacik.daniel@gmail.com>
2020-10-07 21:22:55 +0200

    refactored to app root

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useRoutes } from 'hookrouter';
+import Router from './router';
+import Styled from 'styled-components';
 import * as serviceWorker from './serviceWorker';
 import { ThemeProvider } from 'styled-components';
 
-// components
-import App from './views/App'
-
 // theme
-import theme from './styled-components/theme'
+import theme from './theme/theme'
 
 // style
 import 'sanitize.css';
 import 'sanitize.css/forms.css';
 import 'sanitize.css/typography.css';
 
+const AppRoot = (): JSX.Element => {
+  const router = useRoutes(Router);
+
+  return (
+    <App.Layout>
+      {router}
+    </App.Layout>
+  );
+};
+
+const App = {
+  Layout: Styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    background-color: ${props => props.theme.colors.background};
+    color: ${props => props.theme.colors.light['shade-1']};
+  `
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <AppRoot />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/styled-components/style.d.ts
+++
@@ -1,9 +0,0 @@
-import 'styled-components'
-import theme from './theme'
-
-type CustomTheme = typeof theme
-
-declare module 'styled-components' {
-  // eslint-disable-next-line
-  export interface DefaultTheme extends CustomTheme { }
-}
